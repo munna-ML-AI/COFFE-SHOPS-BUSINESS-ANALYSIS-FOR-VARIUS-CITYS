2de6ae52f2141802db0f4fff481a354484738027
munna-ML-AI <mehedihasanmonna3@gmail.com>
2025-08-25 22:54:25 +0600

    Schemas .sql

--- Schemas (1).sql
+++ Schemas (1).sql
@@ -1,15 +1,10 @@
--- Monday Coffee SCHEMAS
+-- CREATING TABLE AND CONNECTING WITH EACH OTHER
 
 DROP TABLE IF EXISTS sales;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS city;
 
--- Import Rules
--- 1st import to city
--- 2nd import to products
--- 3rd import to customers
--- 4th import to sales
 
 
 CREATE TABLE city
